e4c19d57c161b3e6c7a55d3eea8bb3b3716aed0a
Haykel Ben Jemia <hbj@allmas-tn.com>
2015-10-24 15:44:40 +0100

    Add support for aggregations and simplify metric definition.

--- query-generator.js
+++ query-generator.js
@@ -1,6 +1,24 @@
 'use strict';
 
+var _ = require('lodash');
+
 exports.generate = generate;
+
+// configuration might go in a file or db
+var defaultLevels = {
+  product: 'unit',
+  location: 'store',
+  calendar: 'week'
+};
+
+var aggregationPrimitives = {
+  total: 'TOTAL',
+  min: 'MIN',
+  max: 'MAX',
+  count: 'COUNT'
+};
+
+var aggregations = Object.keys(aggregationPrimitives);
 
 function generate(definitions) {
   if (definitions) {
@@ -26,18 +44,22 @@ function getRequest(definition) {
   var measure = [];
 
   if (definition) {
-    if (definition.metric) {
-      var metrics = Array.isArray(definition.metric) ? definition.metric : [definition.metric];
+    var levels = _.defaults(definition.levels || {}, defaultLevels);
 
-      metrics.forEach(function(metric) {
-        if (metric)
-          measure.push(getMetric(metric));
+    if (definition.measure) {
+      var measureDefinition = Array.isArray(definition.measure) ? definition.measure : [definition.measure];
+
+      measureDefinition.forEach(function(definition) {
+        var m = getMeasure(definition, levels);
+
+        if (m)
+          measure.push(m);
       });
     }
   }
 
   var request = {
-    'return_row_numbers': true
+    return_row_numbers: true
   };
 
   if (measure.length)
@@ -46,9 +68,76 @@ function getRequest(definition) {
   return request;
 }
 
+function getMeasure(definition, levels) {
+  if (typeof definition === 'string')
+    return getMetric(definition);
+
+  var aggregation = null;
+
+  aggregations.some(function(key) {
+    if (definition[key]) {
+      aggregation = key;
+
+      return true;
+    }
+  });
+
+  if (aggregation) {
+    return getAggregation(
+      aggregationPrimitives[aggregation],
+      getMeasure(definition[aggregation], levels),
+      getGrouping(definition.group, levels)
+    );
+  }
+}
+
 function getMetric(name) {
   return {
-    kind: "METRIC",
-    metric: {name: name}
+    kind: 'METRIC',
+    metric: {
+      name: name
+    }
   }
+}
+
+function getAggregation(primitive, expr, grouping) {
+  if (!expr)
+    throw new Error('Aggregation missing expr');
+
+  var aggregation = {
+    method: {
+      primitive: primitive
+    },
+    expr: expr
+  };
+
+  if (grouping)
+    aggregation.grouping = grouping;
+
+  return {
+    kind: 'AGGREGATION',
+    aggregation: aggregation
+  };
+}
+
+function getGrouping(definition, levels) {
+  if (!definition)
+    return null;
+
+  if (!Array.isArray(definition))
+    definition = [definition];
+
+  var grouping = [];
+
+  definition.forEach(function(dimension) {
+    if (levels[dimension]) {
+      grouping.push({
+        kind: 'MAP',
+        dimension: dimension,
+        level: levels[dimension]
+      });
+    }
+  });
+
+  return grouping.length ? grouping : null;
 }
